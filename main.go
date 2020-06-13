94f45eab107d0bc50d0e152501bdc4cbac08099e
samuelvarejames <samuelvarejames@gmail.com>
2020-06-13 21:02:09 +0200

    define function

--- main.go
+++ main.go
@@ -6,3 +6,10 @@ import "fmt"
 func main() {
 	fmt.Println("Let the fun begin")
 }
+
+// A Function to find the shortest path between
+// a given source cell to a destination cell according
+// to A* Search Algorithm
+func aStarSearch() {
+
+}
