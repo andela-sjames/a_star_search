a728583840a267f2c768cfc870c716a6e36b0b82
samuelvarejames <samuelvarejames@gmail.com>
2020-06-14 23:14:53 +0200

    define function

--- main.go
+++ main.go
@@ -10,6 +10,6 @@ func main() {
 // A Function to find the shortest path between
 // a given source cell to a destination cell according
 // to A* Search Algorithm
-func aStarSearch() {
+func aStarSearch(int grid[][COL], Pair src, Pair dest) {
 
 }
