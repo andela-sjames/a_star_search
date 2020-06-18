b8190e8fd3aa1fbf804613a6a05e713c5e8a3242
samuelvarejames <samuelvarejames@gmail.com>
2020-06-18 23:09:49 +0200

    define function

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ func main() {
 	fmt.Println("Let the fun begin")
 }
 
-// A Function to find the shortest path between
+// A function to find the shortest path between
 // a given source cell to a destination cell according
-// to A* Search Algorithm
+// to A* Search Algorithm.
 func aStarSearch() {
 
 }
