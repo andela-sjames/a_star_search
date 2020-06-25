384e91698840254f5b56a861c226dfdd1fc12336
samuelvarejames <samuelvarejames@gmail.com>
2020-06-25 23:10:34 +0200

    Update

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import "fmt"
 
-// A * search
-func main() {
-	fmt.Println("Let the fun begin")
-}
-
 // A function to find the shortest path between
 // a given source cell to a destination cell according
 // to A* Search Algorithm.
 func aStarSearch() {
 
+}
+
+// A * search
+func main() {
+	fmt.Println("Let the fun begin")
 }
