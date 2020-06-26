9b995141aa0bcf0f56329ab709c2fa58f5f6ad73
samuelvarejames <samuelvarejames@gmail.com>
2020-06-26 23:39:28 +0200

    Update

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	utils "github.com/andela-sjames/a_star_search/utils"
+)
 
 // A function to find the shortest path between
 // a given source cell to a destination cell according
@@ -12,4 +16,7 @@ func aStarSearch() {
 // A * search
 func main() {
 	fmt.Println("Let the fun begin")
+	nodezero := utils.NewList("nodezero")
+
+	fmt.Println(nodezero)
 }
