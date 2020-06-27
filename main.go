fc6a5962c1b836c9046cbe9b7626417c757dece5
samuelvarejames <samuelvarejames@gmail.com>
2020-06-27 22:23:15 +0200

    Remove redundant function

--- main.go
+++ main.go
@@ -6,13 +6,6 @@ import (
 	utils "github.com/andela-sjames/a_star_search/utils"
 )
 
-// A function to find the shortest path between
-// a given source cell to a destination cell according
-// to A* Search Algorithm.
-func aStarSearch() {
-
-}
-
 // A * search
 func main() {
 	fmt.Println("Let the fun begin")
