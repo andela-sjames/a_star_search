a5fa6a783e654e1680994174959b554ffd719533
samuelvarejames <samuelvarejames@gmail.com>
2020-06-28 22:11:58 +0200

    Build graph

--- main.go
+++ main.go
@@ -8,8 +8,21 @@ import (
 
 // A * search
 func main() {
-	fmt.Println("Let the fun begin")
 	nodezero := utils.NewList("nodezero")
+	nodezero.AddNode(1, 4)
+	nodezero.AddNode(2, 1)
 
-	fmt.Println(nodezero)
+	nodeone := utils.NewList("nodeone")
+	nodeone.AddNode(3, 1)
+
+	nodetwo := utils.NewList("nodetwo")
+	nodetwo.AddNode(1, 2)
+	nodetwo.AddNode(3, 5)
+
+	nodethree := utils.NewList("nodethree")
+	nodethree.AddNode(4, 3)
+
+	nodefour := utils.NewList("nodefour")
+
+	fmt.Println(nodefour)
 }
