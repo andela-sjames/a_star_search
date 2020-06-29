7a22094265b9729dda756b660da05d9c80aa6a02
samuelvarejames <samuelvarejames@gmail.com>
2020-06-29 23:13:25 +0200

    Add heuristics assuming the euclidean distance

--- utils/list.go
+++ utils/list.go
@@ -10,12 +10,13 @@ type node struct {
 
 // LinkedList struct defined for representing the graph.
 type LinkedList struct {
-	name string
-	head *node
+	name       string
+	heuristics int // let's assume this is the euclidean distance.
+	head       *node
 }
 
 // AddNode adds a node to the linked list data structure.
-func (l *LinkedList) AddNode(vertex, weight int) {
+func (l *LinkedList) AddNode(vertex, weight, heuristics int) {
 	n := &node{
 		vertex: vertex,
 		weight: weight,
@@ -32,8 +33,9 @@ func (l *LinkedList) AddNode(vertex, weight int) {
 }
 
 // NewList used to create a new linked list.
-func NewList(name string) *LinkedList {
+func NewList(name string, heuristics int) *LinkedList {
 	return &LinkedList{
-		name: name,
+		name:       name,
+		heuristics: heuristics, // distance from node to target/goal.
 	}
 }
